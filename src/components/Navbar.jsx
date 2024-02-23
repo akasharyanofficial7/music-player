2a12bc1c0ec78db0e32405a139022e00aad1fec5
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-02-23 15:07:12 +0530

    resolved issue in menu button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { Dropdown } from "flowbite-react";
 
 const Navbar = () => {
   const path = useLocation().pathname;
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // Function to toggle the menu
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   return (
     <nav className="flex justify-between items-center py-4 border-none lg:border px-2 fixed top-0 left-0 right-0 bg-gray-900 z-20">
@@ -45,7 +39,7 @@ const Navbar = () => {
       </div>
 
       {/* 2nd div */}
-      <div className=" flex w-12 lg:w-full  pt-10 lg:h-full h-[70px] lg:pt-0 pl-0 lg:pl-10 lg:flex">
+      <div className=" flex w-12 lg:w-full  pt-10 lg:h-full h-[70px] lg:pt-0 mr-6 pl-0 lg:pl-10 lg:flex">
         <input
           type="text"
           name="search"
@@ -58,55 +52,34 @@ const Navbar = () => {
       </div>
       {/* 3rd div */}
       <div className="flex justify-between items-center gap-4">
-        <div className="flex justify-center gap-1">
-          <div className="flex flex-col text-sm">
-            <span className="text-[15px]  lg:text-[18px] text-white mb-6  lg:mb-0 font-semibold">
-              Languages
-            </span>
-          </div>
+        <div className="flex justify-center gap-1 mb-6 text-white lg:mb-0">
           <div>
-            <div className="cursor-pointer" onClick={toggleMenu}>
-              <MdKeyboardArrowDown
-                className={`text-xl text-white ${
-                  isMenuOpen ? "transform rotate-180" : ""
-                } mt-0 lg:mt-2`}
-              />
-            </div>
+            <Dropdown label="Dropdown" inline>
+              <Dropdown.Header className="font-semibold">
+                music list
+              </Dropdown.Header>
+              <Dropdown.Item>
+                <Link to="/hindi" className="block py-1 hover:text-[#46c7b6ff]">
+                  Hindi
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link to="/english" className="block hover:text-[#46c7b6ff]">
+                  English
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link to="/" className="block hover:text-[#46c7b6ff]">
+                  All song
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+            </Dropdown>
           </div>
-          {/* Dropdown menu for song languages */}
-          {isMenuOpen && (
-            <div className="absolute top-full left-50 w-60 text-center text-sm  bg-white text-black py-2 px-4 shadow-lg rounded-lg">
-              <Link to="/english" className="block py-1 hover:text-[#46c7b6ff]">
-                <hr className=" border-gray-300" />
-                English
-              </Link>
-              <hr className=" border-gray-300" />
-              <Link to="/hindi" className="block py-1 hover:text-[#46c7b6ff]">
-                Hindi
-              </Link>
-              <hr className=" border-gray-300" />
-              <Link to="/" className="block py-1 hover:text-[#46c7b6ff]">
-                Spanish
-              </Link>
-              <hr className=" border-gray-300" />
-              <Link to="/" className="block py-1 hover:text-[#46c7b6ff]">
-                Spanish
-              </Link>
-              <hr className=" border-gray-300" />
-            </div>
-          )}
         </div>
 
         {/* 4th div */}
         <div className="flex text-[12px]  lg:text-[15px] gap-3 pb-6 lg:pb-0 text-white font-semibold">
-          <Link
-            to="/login"
-            className={`flex-nowrap ${
-              path === "/login" ? "text-teal-500" : ""
-            }`}
-          >
-            LogIn
-          </Link>
           <Link
             to="/signup"
             className={`flex-nowrap ${
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -10,79 +10,83 @@ const SignUp = () => {
 
   console.log(formData);
   return (
-    <div className="flex flex-col   pt-12 mx-16 my-16 lg:mx-16 lg:my-16 md:flex-row  justify-center items-center">
-      <div className="w-full md:w-1/3">
-        <div className="flex flex-col justify-center items-center">
-          <div className="self-center whitespace-nowrap my-10 px-4 pb-4  lg:px-6   lg:pb-6 text-sm sm:text-xl font-semibold dark:text-white">
-            <div className="px-2 py-1 text-2xl  flex flex-row lg:text-6xl ">
-              <Link
-                to="/"
-                className="font-extrabold text-lg  lg:text-2xl jiosaavan  font-serif"
-              >
-                <h1>
-                  <span className="font-semibold text-4xl  lg:text-8xl">S</span>
-                  <span className="text-gray-400 text-3xl  lg:text-4xl">
-                    Series
-                  </span>
-                </h1>
-              </Link>
+    <div className="  pt-20">
+      <div className="flex flex-col shadow-md shadow-slate-600 p-4  m-6 rounded-md  pt-12 mx-16 my-16 lg:mx-16 lg:my-16 md:flex-row  justify-center items-center">
+        <div className="w-full md:w-1/3">
+          <div className="flex flex-col justify-center items-center ">
+            <div className="self-center whitespace-nowrap my-10 px-4 pb-4  lg:px-6   lg:pb-6 text-sm sm:text-xl font-semibold dark:text-white">
+              <div className="px-2 py-1 text-2xl  flex flex-row lg:text-6xl ">
+                <Link
+                  to="/"
+                  className="font-extrabold text-lg  lg:text-2xl jiosaavan  font-serif"
+                >
+                  <h1>
+                    <span className="font-semibold text-4xl  lg:text-8xl">
+                      S
+                    </span>
+                    <span className="text-gray-400 text-3xl  lg:text-4xl">
+                      Series
+                    </span>
+                  </h1>
+                </Link>
+              </div>
             </div>
+            <h6 className="text-sm  lg:text-lg">
+              This is a demo project. You can sign up with your email and
+              password or with Google.
+            </h6>
           </div>
-          <h6 className="text-sm  lg:text-lg">
-            This is a demo project. You can sign up with your email and password
-            or with Google.
-          </h6>
         </div>
-      </div>
-      <div className="w-full md:w-2/3 md:ml-8">
-        <div className="max-w-md mx-auto">
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="username" value="Username" />
+        <div className="w-full md:w-2/3 md:ml-8  ">
+          <div className="max-w-md mx-auto">
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="username" value="Username" />
+              </div>
+              <TextInput
+                id="username"
+                type="text"
+                placeholder="Enter your username"
+                required
+                onChange={handleChange}
+              />
             </div>
-            <TextInput
-              id="username"
-              type="text"
-              placeholder="Enter your username"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="email" value="Your email" />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email" value="Your email" />
+              </div>
+              <TextInput
+                id="email"
+                type="email"
+                placeholder="name@flowbite.com"
+                required
+                onChange={handleChange}
+              />
             </div>
-            <TextInput
-              id="email"
-              type="email"
-              placeholder="name@flowbite.com"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="password" value="Your password" />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="password" value="Your password" />
+              </div>
+              <TextInput
+                id="password"
+                type="password"
+                required
+                onChange={handleChange}
+              />
             </div>
-            <TextInput
-              id="password"
-              type="password"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex items-center gap-2 px-2 py-2">
-            <Checkbox id="remember" />
-            <Label htmlFor="remember">Remember me</Label>
-          </div>
-          <Button type="submit" className="w-full">
-            SignUp
-          </Button>
+            <div className="flex items-center gap-2 px-2 py-2">
+              <Checkbox id="remember" />
+              <Label htmlFor="remember">Remember me</Label>
+            </div>
+            <Button type="submit" className="w-full">
+              SignUp
+            </Button>
 
-          <div className="mt-2">
-            <p>
-              Already have an account? <Link to="/login">Sign in</Link>
-            </p>
+            <div className="mt-2">
+              <p>
+                Already have an account? <Link to="/login">Sign in</Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
