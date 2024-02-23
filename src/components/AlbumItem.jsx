c40ea06efa654f7f6de835ecf75c6015860a6459
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-02-23 22:14:49 +0530

    image resize1

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -5,14 +5,14 @@ const AlbumItem = ({ name, artists, id, image }) => {
   return (
     <Link
       to={`/albums/${id}`}
-      className="lg:w-[160px] lg:max-h-[220px] py-10  w-[100px] max-h-[145px] overflow-y-clip flex flex-col justify-center items-center gap-3 rounded-lg"
+      className="lg:w-[160px] lg:max-h-[220px] py-10  w-[100px] max-h-[155px] overflow-y-clip flex flex-col justify-center items-center gap-1  lg:gap-3 rounded-lg"
     >
       <img src={image[2].link} alt="Album Cover" className="rounded-lg" />
       <div className="text-xs w-full flex flex-col justify-center items-center">
         <span className="  font-semibold overflow-x-clip">
           {name.slice(0, 15)}
         </span>
-        <h1 className="  font-thin pb-4 text-xs">
+        <h1 className=" font-thin pb-4 text-xs">
           {artists.map((artist) => artist.name).join(", ").length > 12
             ? artists
                 .map((artist) => artist.name)
